5f1dc887618aa1582e9ff1559545466efe701794
Seung-Hoe Ku <sku@pppl.gov>
2023-07-26 14:30:35 -0400

    Fix finding psi surf in find_line_segment

--- xgc_reader.py
+++ xgc_reader.py
@@ -928,7 +928,7 @@ class xgc1(object):
     # Find line segment of midplane with psi=psi_target or nearest flux surface
     # Works inside separatrix, but not separatrix or SOL
     def find_line_segment(self, n, psi_target, dir='middle'):
-        tmp=np.where(self.mesh.psi_surf/self.psix>psi_target)
+        tmp=np.argmin( (self.mesh.psi_surf/self.psix-psi_target)**2 )
         isurf=tmp[0][0]
         #plt.plot(psi_surf)
         msk=self.mesh.surf_idx[isurf,0:self.mesh.surf_len[isurf]] -1 #node index of the surface, -1 for zero base
