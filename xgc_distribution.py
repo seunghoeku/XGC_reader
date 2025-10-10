Fix the error that b-square has sqrt in calculation -- find_exb_velocity2

xgc_distribution.py
@@ -91,7 +91,8 @@ class XGCDistribution:
     PROTON_MASS: ClassVar[float] = 1.6726219e-27
     E_CHARGE: ClassVar[float] = 1.602176634e-19
     EV_TO_JOULE: ClassVar[float] = 1.602176634e-19
-    MU0_FACTOR: ClassVar[float] = 1/3
+    MU_VOL_FAC: ClassVar[float] = 1/2
+    VP_VOL_FAC: ClassVar[float] = 1/2
     
     #initialize from data passing
     def __init__(self, vgrid: 'VelocityGrid', nnodes: int, f: np.ndarray, den: np.ndarray, temp_ev: np.ndarray, flow: np.ndarray, fg_temp_ev: np.ndarray, mass: float, charge: float, has_maxwellian=True, has_boltzmann=False, dpot=None) -> None:
@@ -238,8 +239,13 @@ class XGCDistribution:
         if(not self.has_maxwellian):
             self.add_maxwellian()
 
-        # apply mu0_factor at zero vperp
-        self.f[:,0,:] = self.f[:,0,:] * self.MU0_FACTOR
+        # apply mu0_factor at boundary of vperp
+        self.f[:,0,:] = self.f[:,0,:] * self.MU_VOL_FAC
+        self.f[:,-1,:] = self.f[:,-1,:] * self.MU_VOL_FAC
+        
+        #apply vp_vol_fac at boundary of vpara
+        self.f[:,:,0] = self.f[:,:,0] * self.VP_VOL_FAC
+        self.f[:,:,1] = self.f[:,:,1] * self.VP_VOL_FAC
 
         #get vspace volume
         vspace_vol = self.fg_temp_ev * np.sqrt(1/(np.pi*2)) * self.vgrid.dvperp * self.vgrid.dvpara
@@ -278,8 +284,11 @@ class XGCDistribution:
                 print('flow has nan after flux average')
 
 
-        # undo multiplication by MU0_FACTOR
-        self.f[:,0,:] = self.f[:,0,:] / self.MU0_FACTOR
+        # undo multiplication by MU_VOL_FAC
+        self.f[:,0,:] = self.f[:,0,:] / self.MU_VOL_FAC
+        self.f[:,-1,:] = self.f[:,-1,:] / self.MU_VOL_FAC
+        self.f[:,:,0] = self.f[:,:,0] / self.VP_VOL_FAC
+        self.f[:,:,1] = self.f[:,:,1] / self.VP_VOL_FAC
 
         # update maxwellian moments
         if(no_update):

xgc_reader/analysis.py
@@ -278,7 +278,7 @@ and calculate ExB velocity in time.
 def find_exb_velocity2(xgc_instance, istart, iend, skip, ms, only_average=True):
 
     bt = xgc_instance.bfield[2,ms]
-    b2 = np.sqrt(xgc_instance.bfield[0,ms]**2 + xgc_instance.bfield[1,ms]**2 + xgc_instance.bfield[2,ms]**2)
+    b2 = xgc_instance.bfield[0,ms]**2 + xgc_instance.bfield[1,ms]**2 + xgc_instance.bfield[2,ms]**2
 
     pbar = tqdm(range(istart,iend,skip))
     for count, istep in enumerate(pbar):

xgc_reader/base.py
@@ -275,12 +275,19 @@ class xgc1(object):
         with adios2.FileReader(self.path + "xgc.bfield.bp") as f:
             try:
                 self.bfield = f.read('bfield')
+            except: # try older version of bfield
+                self.bfield = f.read('/node_data[0]/values')
+
+            if(self.bfield.shape[0]!=3): # not 3xN
+                self.bfield = np.transpose(self.bfield)
+                print('bfield shape is :', self.bfield.shape)            
+    
+            try:
+                self.jpar_bg = f.read('jpar_bg') # background current
             except:
-                try:
-                    self.bfield = f.read('/bfield')
-                except:
-                    print("Could not read bfield data")
-                    self.bfield = None
+                print('No jpar_bg in xgc.bfield.bp')
+
+
 
     def load_heatdiag(self, **kwargs):
         """Load heat diagnostic data."""

xgc_reader_old.py
@@ -1586,7 +1586,7 @@ class xgc1(object):
     def find_exb_velocity2(self, istart, iend, skip, ms, only_average=True):
 
         bt = self.bfield[2,ms]
-        b2 = np.sqrt(self.bfield[0,ms]**2 + self.bfield[1,ms]**2 + self.bfield[2,ms]**2)
+        b2 = (self.bfield[0,ms]**2 + self.bfield[1,ms]**2 + self.bfield[2,ms]**2)
 
         pbar = tqdm(range(istart,iend,skip))
         for count, istep in enumerate(pbar):